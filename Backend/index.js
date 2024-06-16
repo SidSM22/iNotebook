1e802956e217d1cb78d25ee2d806e40a19dc18be
SidSM22 <145437512+SidSM22@users.noreply.github.com>
2024-06-17 01:00:04 +0530

    made user to Save its data in MongoDB server

--- Backend/index.js
+++ Backend/index.js
@@ -6,9 +6,12 @@ const connectToMongo = require('./db')
  const app = express();
  const port = 3000;
  
+ app.use(express.json());
  //available default routes
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
+
+
 
  app.get("/",(req,res) =>{
     res.send("Hello World");
--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-
+const { Schema } = mongoose; 
 const UserSchema = new Schema({
     name:{
         type: String,
@@ -16,8 +16,8 @@ const UserSchema = new Schema({
     },
     date:{
         type: Date,
-        required: Date.now
-    }
+        default: Date.now
+    },
 });
 
 module.exports = mongoose.model('user',UserSchema)
