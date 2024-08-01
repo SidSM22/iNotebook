095a8d80e504890b49c79867a7ddd4f7532c7251
SidSM22 <145437512+SidSM22@users.noreply.github.com>
2024-08-01 14:58:11 +0530

    Context API

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import About from "./components/About";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import NoteState from './context/notes/NoteState';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
+    <NoteState>
       <BrowserRouter>
         <Navbar />
         <Routes>
@@ -15,6 +17,7 @@ function App() {
           <Route exact path="/about" element={<About/>}></Route>
         </Routes>
       </BrowserRouter>
+      </NoteState>
     </>
   );
 }
