34dffd4abee1f6f1371d318a5009fc8194423674
SidSM22 <145437512+SidSM22@users.noreply.github.com>
2024-08-01 15:37:36 +0530

    Done with creating form

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,9 @@
 
-import React, { useContext, useEffect } from 'react'
-import noteContext from '../context/notes/noteContext'
-const About = () => {
-  const a = useContext(noteContext);
-  useEffect(() => {
-    a.update();
-    // eslint-disable-next-line
-  }, [])
-
+// import React, { useContext, useEffect } from 'react'
+const About = () => { 
   return (
     <div>
-      This is About {a.state.name} in {a.state.class}
+      This is About page
     </div>
   )
 }
