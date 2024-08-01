3b8c67a198e49fba5deeefeecb3729ee1fe30826
SidSM22 <145437512+SidSM22@users.noreply.github.com>
2024-08-01 21:57:00 +0530

    created addnote component

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -70,8 +70,33 @@ const NoteState = (props) =>{
 
     const [notes, setNotes] = useState(notesInitial);
 
+    //Add a note
+    const addNote = (title, description, tag)=>{
+      //to do api calls
+      console.log("Adding a new note")
+     const note = {
+        "_id": "66a4e29485cbaea496511ae7",
+        "user": "66700a16fbaddbc7821c4fa12",
+        "title": title,
+        "description": description,
+        "tag": tag,
+        "date": "2024-07-27T12:05:40.198Z",
+        "__v": 0
+        }
+      setNotes(notes.concat(note))
+    }
+    //Delete a note
+    const deleteNote = (id)=>{
+      
+    }
+    
+    //Edit a note
+    const editNote = (id)=>{
+      
+    }
+
     return (
-        <NoteContext.Provider value={{notes, setNotes}}>
+        <NoteContext.Provider value={{notes, addNote, deleteNote, editNote}}>
             {props.children}
         </NoteContext.Provider>
     )
