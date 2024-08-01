d71a72b20a8611ece959b175af42831ddb9906cc
SidSM22 <145437512+SidSM22@users.noreply.github.com>
2024-08-01 20:51:11 +0530

    created noteitem and notes component

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -20,7 +20,52 @@ const NoteState = (props) =>{
           "tag": "personal",
           "date": "2024-07-27T12:05:40.198Z",
           "__v": 0
-        }
+        },
+        {
+            "_id": "66a4e29495cbaea496511ae7",
+            "user": "66700a16fbaddbc7821c4fa1",
+            "title": "My Title",
+            "description": "This is the Description of Note",
+            "tag": "personal",
+            "date": "2024-07-27T12:05:40.198Z",
+            "__v": 0
+        },
+        {
+            "_id": "668175f8231316fa3be1b65b",
+            "user": "66700a16fbaddbc7821c4fa1",
+            "title": "Updated banda hu",
+            "description": "Ghar ka na ghat ka",
+            "tag": "public",
+            "date": "2024-06-30T15:12:56.206Z",
+            "__v": 0
+          },
+          {
+            "_id": "66a4e29495cbaea496511ae7",
+            "user": "66700a16fbaddbc7821c4fa1",
+            "title": "My Title",
+            "description": "This is the Description of Note",
+            "tag": "personal",
+            "date": "2024-07-27T12:05:40.198Z",
+            "__v": 0
+          },
+          {
+              "_id": "66a4e29495cbaea496511ae7",
+              "user": "66700a16fbaddbc7821c4fa1",
+              "title": "My Title",
+              "description": "This is the Description of Note",
+              "tag": "personal",
+              "date": "2024-07-27T12:05:40.198Z",
+              "__v": 0
+            },
+        {
+            "_id": "66a4e29495cbaea496511ae7",
+            "user": "66700a16fbaddbc7821c4fa1",
+            "title": "My Title",
+            "description": "This is the Description of Note",
+            "tag": "personal",
+            "date": "2024-07-27T12:05:40.198Z",
+            "__v": 0
+            }
       ]   
 
     const [notes, setNotes] = useState(notesInitial);
